18ffe8d65c2dd6d53d956ec7c4a30e20865bf47c
Miguel Piedrafita <github@miguel.build>
2023-08-04 16:30:37 +0100

    Empty state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,6 +42,7 @@ const Commit = () => {
 	const commit = useCallback(
 		(event: FormEvent<HTMLFormElement>) => {
 			event.preventDefault()
+			if (diff?.files_changed === 0) return
 
 			invoke('commit', { path, title, description: description.trim() == '' ? null : description.trim() }).catch(
 				error => alert(error)
@@ -133,62 +134,81 @@ const Commit = () => {
 				/>
 			</div>
 			<div className="flex items-center justify-between border-t dark:border-white/5 p-4 bg-white/90 dark:bg-black/10">
-				<div className="flex items-center space-x-4">
-					<p className="text-black/40 dark:text-white/50">
-						<AnimatePresence mode="popLayout">
-							<motion.span layout>{diff?.files_changed ?? '??'}</motion.span>
-						</AnimatePresence>{' '}
-						file{diff?.files_changed == 1 ? '' : 's'} changed
-					</p>
+				<AnimatePresence mode="popLayout">
+					{diff?.files_changed === 0 ? (
+						<motion.p
+							key="no-changes"
+							exit={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							initial={{ opacity: 0 }}
+							className="text-black/40 dark:text-white/50"
+						>
+							No changes detected
+						</motion.p>
+					) : (
+						<div className="flex items-center space-x-4">
+							<p className="text-black/40 dark:text-white/50">
+								<AnimatePresence mode="popLayout">
+									<motion.span layout>{diff?.files_changed ?? '??'}</motion.span>
+								</AnimatePresence>{' '}
+								file{diff?.files_changed == 1 ? '' : 's'} changed
+							</p>
 
-					<div className="h-6 w-px bg-black/10 dark:bg-white/5" />
-					<div className="flex items-center space-x-3">
-						<div className="flex items-center space-x-2">
-							<AnimatePresence>
-								{diff?.insertions != null && (
-									<motion.p
-										exit={{ opacity: 0 }}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										className="text-green-600"
-									>
-										+{diff.insertions}{' '}
-									</motion.p>
-								)}
-							</AnimatePresence>
-							<AnimatePresence mode="popLayout">
-								{diff?.deletions != null && (
-									<motion.p
-										exit={{ opacity: 0 }}
-										key="active-deletions"
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										className="text-red-600"
-									>
-										{' '}
-										-{diff.deletions}
-									</motion.p>
-								)}
-							</AnimatePresence>
-						</div>
-						{/* <div className="flex items-center space-x-[2px]">
+							<div className="h-6 w-px bg-black/10 dark:bg-white/5" />
+							<div className="flex items-center space-x-3">
+								<div className="flex items-center space-x-2">
+									<AnimatePresence>
+										{diff?.insertions != null && (
+											<motion.p
+												exit={{ opacity: 0 }}
+												initial={{ opacity: 0 }}
+												animate={{ opacity: 1 }}
+												className="text-green-600"
+											>
+												+{diff.insertions}{' '}
+											</motion.p>
+										)}
+									</AnimatePresence>
+									<AnimatePresence mode="popLayout">
+										{diff?.deletions != null && (
+											<motion.p
+												exit={{ opacity: 0 }}
+												key="active-deletions"
+												initial={{ opacity: 0 }}
+												animate={{ opacity: 1 }}
+												className="text-red-600"
+											>
+												{' '}
+												-{diff.deletions}
+											</motion.p>
+										)}
+									</AnimatePresence>
+								</div>
+								{/* <div className="flex items-center space-x-[2px]">
 							<div className="rounded w-3.5 h-3.5 bg-emerald-500" />
 							<div className="rounded w-3.5 h-3.5 bg-red-600/90" />
 							<div className="rounded w-3.5 h-3.5 bg-red-600/90" />
 							<div className="rounded w-3.5 h-3.5 bg-red-600/90" />
 						</div> */}
-					</div>
-				</div>
+							</div>
+						</div>
+					)}
+				</AnimatePresence>
 				<div>
-					<button
-						className="bg-white dark:bg-white/10 border dark:border-white/10 shadow rounded-lg flex items-center space-x-2 py-1 px-2"
-						type="submit"
-					>
-						<p className="font-medium text-black/80 dark:text-white/80">Commit</p>
-						<span className="text-sm rounded border dark:border-white/5 bg-gray-100 dark:bg-white/5 px-1 -mx-1 text-black/50 dark:text-white/50">
-							⌘⏎
-						</span>
-					</button>
+					{diff?.files_changed !== 0 && (
+						<motion.button
+							type="submit"
+							exit={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							initial={{ opacity: 0 }}
+							className="bg-white dark:bg-white/10 border dark:border-white/10 shadow rounded-lg flex items-center space-x-2 py-1 px-2"
+						>
+							<p className="font-medium text-black/80 dark:text-white/80">Commit</p>
+							<span className="text-sm rounded border dark:border-white/5 bg-gray-100 dark:bg-white/5 px-1 -mx-1 text-black/50 dark:text-white/50">
+								⌘⏎
+							</span>
+						</motion.button>
+					)}
 				</div>
 			</div>
 		</form>
